7163ed59696370bbb9b5fe972c34d95515b33ceb
Estel Smith <estel.smith@gmail.com>
2020-05-11 14:37:22 -0400

    add beginning of vue app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-//@todo
+import App from '@/app.vue';
+import Vue from 'vue';
+
+new Vue({
+    components: {
+        App
+    },
+    el: 'vue-app',
+    render: (h) => h(App)
+});
---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="app">
+        <span>{{ msg }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+    import Component from 'vue-class-component';
+    import Vue from 'vue';
+
+    @Component
+    export default class App extends Vue {
+        msg: string = 'App loaded.';
+    };
+</script>
